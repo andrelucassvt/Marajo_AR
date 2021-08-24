339fc8f2bb5a8296eb2d865ee206d0b638393c18
andrelucassvt <andrelucassvt99@gmail.com>
2021-08-24 11:18:16 -0300

    grandes modificacoes

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:marajoar/app/core/colors.dart';
 import 'package:marajoar/app/modules/home/home_page.dart';
 import 'package:marajoar/app/modules/home/pages/sobre/sobre_page.dart';
-import 'package:marajoar/app/modules/start/start_page.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'package:responsive_framework/responsive_wrapper.dart';
 
@@ -16,14 +15,14 @@ class AppWidget extends StatelessWidget {
         '/sobre': (_) => SobrePage()
       },
       builder: (context, widget) => ResponsiveWrapper.builder(
-          ClampingScrollWrapper.builder(context,widget),
-          minWidth: 450,
-          defaultScale: true,
-          breakpoints: [
-            ResponsiveBreakpoint.resize(450, name: MOBILE),
-            ResponsiveBreakpoint.autoScale(800, name: TABLET),
-          ],
-          background: Container(color: Color(0xFFF5F5F5))),
+        ClampingScrollWrapper.builder(context,widget),
+        minWidth: 450,
+        defaultScale: true,
+        breakpoints: [
+          ResponsiveBreakpoint.resize(450, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(800, name: TABLET),
+        ],
+        background: Container(color: Color(0xFFF5F5F5))),
     );
   }
 }
--- lib/app/modules/home/service/home_service.dart
+++ lib/app/modules/home/service/home_service.dart
@@ -16,7 +16,7 @@ class HomeService {
         nome: 'Vaso Marajoara',
         foto: 'assets/objetos/vaso_marajoara.jpg',
         descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-        objeto: 'vaso.sfb',
+        objeto: 'vaso2.sfb',
       ),
     ];
     return lista;  
