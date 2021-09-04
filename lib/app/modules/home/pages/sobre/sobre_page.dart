6c9b4a32bc291eefa204936364d2d6500f7e5389
andresalvador-vwi <58487111+andrelucassvt@users.noreply.github.com>
2021-09-03 22:06:24 -0300

    nova imagem na tela sobre

--- lib/app/modules/home/pages/sobre/sobre_page.dart
+++ lib/app/modules/home/pages/sobre/sobre_page.dart
@@ -16,9 +16,38 @@ class SobrePageState extends State <SobrePage> {
         title: Text(locale.HomePagesSobreAppBarTitle),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[],
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/buffaloSobre.png')
+                )
+              ),
+            ),
+            Text(
+              textoBR,
+              style: TextStyle(
+                fontSize: 20
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+String textoBR = """
+Marajó AR (Marajó em realidade aumentada), tem como objetivo levar um pouco da cultura da marajoara através da realidade aumentada. A ideia surgiu para dar mais visibilidade à maior ilha fluviomarítima do Brasil e do Mundo. 
+
+O projeto foi desenvolvido a todo esforço (sem apoio político) por duas pessoas que amam a Ilha do Marajó, terra onde nasceram e cresceram.
+As duas pessoas por trás desse projeto são:
+André Salvador, analista de sistemas.
+Wendel Alves, estudante de engenharia de petróleo.
+
+Esse projeto teve apoio indispensável do professor universitário da UNAMA (Universidade da Amazônia) Alan Marciel de Souza. O professor acreditou na ideia e ajudou a registrar o sistema no INPI (Instituto nacional da propriedade industrial).
+""";
