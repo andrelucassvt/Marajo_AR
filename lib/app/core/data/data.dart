177e3db5941b15e0ec2068db8653c816626dc926
andresalvador-vwi <81233338+andresalvador-vwi@users.noreply.github.com>
2021-09-23 16:59:25 -0300

    bug do queijo resolvido IOS

--- lib/app/core/data/data.dart
+++ lib/app/core/data/data.dart
@@ -27,7 +27,7 @@ class LanguageListArModel {
         nome: locale.CoreDataListQueijoName,
         foto: 'assets/comidas/queijo.jpg',
         descricao: locale.CoreDataListQueijoDesc,
-        objeto: Platform.isIOS ? 'models.scnassets/queijom.usdz' : 'queijo.sfb',
+        objeto: Platform.isIOS ? 'models.scnassets/queijom2.usdz' : 'queijo.sfb',
         isfavorito: true,
         categoriasEnum: CategoriasEnum.comidas,
         listaImagens: [
