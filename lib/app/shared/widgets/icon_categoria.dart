a3a7676368ae682dd8574fd0dacd15166a468e9c
andresalvador-vwi <81233338+andresalvador-vwi@users.noreply.github.com>
2022-01-17 13:31:21 -0300

    correcao para o beta

--- lib/app/shared/widgets/icon_categoria.dart
+++ lib/app/shared/widgets/icon_categoria.dart
@@ -1,6 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:marajoar/app/shared/core/colors.dart';
 
 class IconWidgetCategoria extends StatelessWidget {
   final String nome;
@@ -17,7 +16,7 @@ class IconWidgetCategoria extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => this.ontap,
+      onTap: this.ontap,
       child: Container(
         width: 100,
         padding: EdgeInsets.all(10),
