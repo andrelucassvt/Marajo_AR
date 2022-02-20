8c6b3c49d9afc97f226f2e71b697555941bb05d9
andresalvador-vwi <58487111+andrelucassvt@users.noreply.github.com>
2022-02-19 23:33:55 -0300

    correcoes de bugs

--- lib/app/modules/home/presenter/blocs/home_enable_filter/home_enable_filter_cubit.dart
+++ lib/app/modules/home/presenter/blocs/home_enable_filter/home_enable_filter_cubit.dart
@@ -7,7 +7,7 @@ part 'home_enable_filter_state.dart';
 class HomeEnableFilterCubit extends Cubit<HomeEnableFilterState> {
   HomeEnableFilterCubit() : super(HomeEnableFilterFalse());
 
-  Future<void> enableFilter(){
+  Future<void> enableFilter() async {
     if (state is HomeEnableFilterTrue) {
       emit(HomeEnableFilterFalse());
     } else if (state is HomeEnableFilterFalse) {
